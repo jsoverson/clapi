802cd28a16125663225489c8ad9935e69ddb131b
Jarrod Overson <jsoverson@gmail.com>
2015-05-06 19:16:12 -0700

    exposing command

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 import Application from './application';
+import Command from './command';
 import Input from './input';
 import Output from './output';
 
 export default {
   create : Application.init.bind(Application),
   Application,
+  Command,
   Input,
   Output
 };
