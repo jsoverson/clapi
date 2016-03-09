e1a8bcf9ea421e6bd4d225adff8a7a8cf7fde83d
Jarrod Overson <jsoverson@gmail.com>
2016-03-08 20:36:18 -0800

    added tests

--- test/command.spec.js
+++ test/command.spec.js
@@ -50,86 +50,120 @@ describe('command', () => {
       done();
     });
   });
-  it('should run middleware before before, after, and run', function(done){
-    command.use({
-      before: function(input, output, done) {
-        input.middlewareBefore = 1;
-        output.middlewareBefore = 10;
+  describe('middleware', function(){
+    it('should run middleware even without task handlers defined', function(done){
+      command.use({
+        before: function(input, output, done) {
+          input.middlewareBefore = 1;
+          output.middlewareBefore = 10;
+          done();
+        },
+        run: function(input, output, done) {
+          input.middlewareRun = 2;
+          output.middlewareRun = 20;
+          done();
+        },
+        after: function(input, output, done) {
+          input.middlewareAfter = 3;
+          output.middlewareAfter = 30;
+          done();
+        }
+      });
+      command.run([{},{}], function(err, input, output) {
+        if (err) return done(err);
+        assert.equal(input.middlewareBefore, 1);
+        assert.equal(output.middlewareBefore, 10);
+        assert.equal(input.middlewareRun, 2);
+        assert.equal(output.middlewareRun, 20);
+        assert.equal(input.middlewareAfter, 3);
+        assert.equal(output.middlewareAfter, 30);
         done();
-      },
-      run: function(input, output, done) {
-        input.middlewareRun = 2;
-        output.middlewareRun = 20;
-        done();
-      },
-      after: function(input, output, done) {
-        input.middlewareAfter = 3;
-        output.middlewareAfter = 30;
-        done();
-      }
+      });
     });
-    command.add((input, output, done) => {
-      assert.equal(input.middlewareBefore, 1);
-      assert.equal(output.middlewareBefore, 10);
-      assert.equal(input.middlewareRun, 2);
-      assert.equal(output.middlewareRun, 20);
-      assert(!input.middlewareAfter);
-      assert(!output.middlewareAfter);
-      input.primary = 100;
-      output.primary = 200;
-      done();
+    it('should run middleware before before, after, and run', function(done){
+      command.use({
+        before: function(input, output, done) {
+          input.middlewareBefore = 1;
+          output.middlewareBefore = 10;
+          done();
+        },
+        run: function(input, output, done) {
+          input.middlewareRun = 2;
+          output.middlewareRun = 20;
+          done();
+        },
+        after: function(input, output, done) {
+          input.middlewareAfter = 3;
+          output.middlewareAfter = 30;
+          done();
+        }
+      });
+      command.add((input, output, done) => {
+        assert.equal(input.middlewareBefore, 1);
+        assert.equal(output.middlewareBefore, 10);
+        assert.equal(input.middlewareRun, 2);
+        assert.equal(output.middlewareRun, 20);
+        assert(!input.middlewareAfter);
+        assert(!output.middlewareAfter);
+        input.primary = 100;
+        output.primary = 200;
+        done();
+      });
+      command.run([{},{}], function(err, input, output) {
+        if (err) return done(err);
+        assert.equal(input.middlewareAfter, 3);
+        assert.equal(output.middlewareAfter, 30);
+        assert.equal(input.primary, 100);
+        assert.equal(output.primary, 200);
+        done();
+      });
     });
-    command.run([{},{}], function(err, input, output) {
-      if (err) return done(err);
-      assert.equal(input.middlewareAfter, 3);
-      assert.equal(output.middlewareAfter, 30);
-      assert.equal(input.primary, 100);
-      assert.equal(output.primary, 200);
-      done();
+    it('should not freak out on missing methods in middleware', function(done){
+      command.use({
+        run: function(input, output, done) {
+          input.middlewareRun = 2;
+          output.middlewareRun = 20;
+          done();
+        },
+      });
+      command.add((input, output, done) => {
+        assert.equal(input.middlewareRun, 2);
+        assert.equal(output.middlewareRun, 20);
+        input.primary = 100;
+        output.primary = 200;
+        done();
+      });
+      command.run([{},{}], function(err, input, output) {
+        if (err) return done(err);
+        assert.equal(input.primary, 100);
+        assert.equal(output.primary, 200);
+        done();
+      });
     });
   });
-  it('should not freak out on missing methods in middleware', function(done){
-    command.use({
-      run: function(input, output, done) {
-        input.middlewareRun = 2;
-        output.middlewareRun = 20;
+  describe('subcommands', function(){
+    it('should create and run subcommands', (done) => {
+      command.command('pull').add((input, output, done) => {output.a = 10; done();} )
+        .add((input, output, done) => {output.b = 100; done();} );
+      command.run('pull', (err, input, output) => {
+        if (err) return done(err);
+        assert.equal(output.a, 10);
+        assert.equal(output.b, 100);
         done();
-      },
+      });
     });
-    command.add((input, output, done) => {
-      assert.equal(input.middlewareRun, 2);
-      assert.equal(output.middlewareRun, 20);
-      input.primary = 100;
-      output.primary = 200;
-      done();
-    });
-    command.run([{},{}], function(err, input, output) {
-      if (err) return done(err);
-      assert.equal(input.primary, 100);
-      assert.equal(output.primary, 200);
-      done();
+    it('should allow global middleware to augment input & output', (done) => {
+      command.before((input, output, pluginDone) => {
+        output.log = (value) => {
+          assert.equal(2, value);
+        };
+        pluginDone();
+      });
+      command.command('pull').add((input, output) => {output.log(2);} );
+      command.run('pull', [{}, {}], done);
     });
   });
-  it('should create and run subcommands', (done) => {
-    command.command('pull').add((input, output, done) => {output.a = 10; done();} )
-      .add((input, output, done) => {output.b = 100; done();} );
-    command.run('pull', (err, input, output) => {
-      if (err) return done(err);
-      assert.equal(output.a, 10);
-      assert.equal(output.b, 100);
-      done();
-    });
-  });
-  it('should allow global middleware to augment input & output', (done) => {
-    command.before((input, output, pluginDone) => {
-      output.log = (value) => {
-        assert.equal(2, value);
-      };
-      pluginDone();
-    });
-    command.command('pull').add((input, output) => {output.log(2);} );
-    command.run('pull', [{}, {}], done);
-  });
+
   it('should pass input & output to after', (done) => {
     var ran = false;
     command.after((input, output, pluginDone) => {
